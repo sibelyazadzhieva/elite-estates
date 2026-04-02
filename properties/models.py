38d6be1928a35b7eec074f85523f9fdf6716c90d
Sibel Yazadzhieva <qzadjieva.sibel@gmail.com>
2026-04-02 18:30:13 +0300

    Create properties app with Feature and Property models

---
+++ properties/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class Feature(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Feature Name")
+
+    def __str__(self):
+        return self.name
+
+class Property(models.Model):
+    PROPERTY_TYPES = [
+        ('apartment', 'Apartment'),
+        ('house', 'House'),
+        ('commercial', 'Commercial'),
+    ]
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    property_type = models.CharField(max_length=20, choices=PROPERTY_TYPES)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='property_images/', blank=True, null=True)
+    agent = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='properties')
+    features = models.ManyToManyField(Feature, related_name='properties', blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.location}"
