2effd2913a4d4a455cd696512b81df6f04f0ada9
Sibel Yazadzhieva <qzadjieva.sibel@gmail.com>
2026-04-03 22:44:42 +0300

    Implement user authentication: base template, registration and login functionality

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterUserView, CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class RegisterUserView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
