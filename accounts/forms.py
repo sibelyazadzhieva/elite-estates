d440a8cb7b88174f6dfc29a581fcdb36b8d2d761
Sibel Yazadzhieva <qzadjieva.sibel@gmail.com>
2026-04-07 03:10:51 +0300

    Implement environment variables for security and update requirements

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 
 UserModel = get_user_model()
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = UserModel
         fields = ('username', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = "Required. 6 characters or more. Letters and digits only."
+        self.fields['username'].validators.append(MinLengthValidator(6))
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
--- elite_estates/settings.py
+++ elite_estates/settings.py
@@ -12,19 +12,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-satp13xnx@_eefhl6vixzdl-i!td)o6l97t$eh=m4#p%%xb)7m'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
