a4a46573dede1984b4ba02f69ed4eac5a87deab9
Sibel Yazadzhieva <qzadjieva.sibel@gmail.com>
2026-04-20 22:14:08 +0300

    Setup Celery and Redis for advanced asynchronous background tasks

--- elite_estates/settings.py
+++ elite_estates/settings.py
@@ -151,3 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
